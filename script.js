747d56e10a39e5f2011a182e4c3950a7cb9641de
CoderTeam2 <72189661+CoderTeam2@users.noreply.github.com>
2021-05-15 18:59:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function addItem(){
+    var item = document.createElement("LI");
+    item.innerHTML = document.getElementById("text").value + " ";
+    document.getElementById("text").value = "";
+    var list = document.getElementById("list");
+    var button = document.createElement("BUTTON");
+    button.innerHTML = "❌";
+    button.addEventListener("click",function(){
+        item.remove();
+    })
+    item.appendChild(button);
+    list.appendChild(item);
+}
