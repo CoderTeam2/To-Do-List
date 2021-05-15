3af4240efb70f5f03d69a610c416f3f07d82650a
CoderTeam2 <72189661+CoderTeam2@users.noreply.github.com>
2021-05-15 18:56:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function addItem(){
+    var x = document.getElementById("text");
     var item = document.createElement("LI");
-    item.innerHTML = document.getElementById("text").value + " ";
+    item.innerHTML = x.value + " ";
+    x.value = "";
     var list = document.getElementById("list");
     var button = document.createElement("BUTTON");
     button.innerHTML = "❌";
